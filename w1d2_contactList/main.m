2b181d023311b2414bb72bfb65c6c701bc18981b
hkimca <harrison.kim.ca@gmail.com>
2017-06-27 15:07:10 -0400

    Initial Commit

---
+++ w1d2_contactList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  w1d2_contactList
+//
+//  Created by Seantastic31 on 27/06/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
